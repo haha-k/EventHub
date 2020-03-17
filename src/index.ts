d7f022511d96f32a2012587592bb4715bb7aec8c
hahakk <afyapu@163.com>
2020-03-18 02:11:26 +0800

    :sparkles:新增off

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,35 @@
 export default class EventHub {
-  cache = {};
+  private cache = {};
   on(eventName, fn) {
     //把fn推进this.cache[eventName]
     this.cache[eventName] = this.cache[eventName] || [];
     this.cache[eventName].push(fn);
   }
-  emit(eventName) {
+  emit(eventName, data?) {
     //把this.cache[eventName]
-    (this.cache[eventName] || []).forEach(fn => fn());
+    (this.cache[eventName] || []).forEach(fn => fn(data));
   }
+  off(eventName, fn) {
+    let index = indexOf(this.cache[eventName], fn);
+    if (index === -1) return;
+    this.cache[eventName].splice(index, 1);
+  }
+}
+
+/**
+ *  帮助函数 indexOf
+ * @param array
+ * @param item
+ */
+function indexOf(array, item) {
+  if (array === undefined) {
+    return -1;
+  }
+  let index = -1;
+  for (let i = 0; i < array.length; ++i) {
+    if (array[i] === item) {
+      index = i;
+    }
+  }
+  return index;
 }
--- test/index.ts
+++ test/index.ts
@@ -1,13 +1,40 @@
 import EventHub from "../src/index";
 
+
+const test1 = () => {
 const eventHub = new EventHub();
+  console.assert(eventHub instanceof Object === true);
+};
 
-console.assert(eventHub instanceof Object === true);
-
-//on emit
+const test2 = ()=>{
+const eventHub = new EventHub();
 let called = false;
-eventHub.on("xxx", () => {
+eventHub.on("xxx", data => {
   called = true;
-  console.log("调用");
+  console.assert(data === "haha");
 });
-eventHub.emit("xxx");
+eventHub.emit("xxx", "haha");
+setTimeout(() => {
+    console.assert(called === true);
+}, 1000);
+}
+//on emit
+
+const test3 = () => {
+const eventHub = new EventHub();
+let called = false;
+const fn1 = () => {
+  called = true;
+};
+eventHub.on("yyy", fn1);
+eventHub.off("yyy", fn1);
+eventHub.emit("yyy");
+setTimeout(() => {
+    console.assert(called === false);
+}, 1000);
+
+}
+
+test1();
+test2();
+test3();
